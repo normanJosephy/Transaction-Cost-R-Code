302eb0d0e04555e723a27ae875773fca56db9fb4
normanJosephy <njosephy@bentley.edu>
2014-05-16 13:53:36 -0400

    initial commit of myMultiStart.R and solvingForContour.R

---
+++ myMultiStart.R
@@ -0,0 +1,29 @@
+
+# myMultiStart.R  5/12/2014
+
+require(doParallel)
+require(foreach)
+
+myMultiStart = function (par, fn, control = list()) 
+{
+  pmat <- matrix(NA, nrow(par), ncol(par))
+  for (k in 1:nrow(par)) {
+      ans <- try(BBsolve(par[k, ], 
+                         fn = fn , 
+                         quiet = TRUE,
+                         control = list()), silent = TRUE)
+    if (inherits(ans, "try-error")) 
+      next
+    pmat[k, ] <- ans$par
+  }
+    pmat
+}
+
+myMultiStartParallel = function (par, fn, control = list(), ...) 
+{
+  foreach(k = 1:nrow(par),.combine=rbind) %do%
+    BBsolve(par[k, ], 
+            fn = fn , 
+            quiet = TRUE,
+            control = list())$par
+  }
